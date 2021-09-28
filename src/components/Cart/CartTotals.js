cbbfb1abe61a1327a9dfa905a5361dc5b699cfb4
akhandpratapbhu <akhandpratap121196@gmail.com>
2021-09-29 04:21:35 +0530

    first commit

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import PayPalButton from './PayPalButton';
 
 export default function CartTotals({value,history}) {
     const {cartSubTotal,cartTax,cartTotal,clearCart}=value;
@@ -34,6 +35,8 @@ export default function CartTotals({value,history}) {
                             <strong>${cartTotal}</strong>
                         </h5>
                        
+                       <PayPalButton total={cartTotal} clearCart={clearCart}
+                       history={history}/>
                     </div>
                     
                 </div>
